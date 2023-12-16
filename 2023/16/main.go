605450e4aa1b88057d9f964ba831f4af3029ffcf
Alex Houseago <ahouseago@gmail.com>
2023-12-16 22:25:13 +0000

    Might as well use array as map keys

--- 2023/16/main.go
+++ 2023/16/main.go
@@ -10,8 +10,91 @@ import (
 var input string
 
 type node struct {
-	pos []int
+	pos [2]int
 	dir string
+}
+
+func main() {
+	grid := strings.Split(strings.TrimSpace(input), "\n")
+	fmt.Println("Part 1:", energised(grid, node{[2]int{0, 0}, "R"}))
+
+	highest := 0
+	for i := 0; i < len(grid); i++ {
+		highest = max(energised(grid, node{[2]int{i, 0}, "R"}), highest)
+		highest = max(energised(grid, node{[2]int{i, len(grid[0]) - 1}, "L"}), highest)
+	}
+	for i := 0; i < len(grid[0]); i++ {
+		highest = max(energised(grid, node{[2]int{0, i}, "D"}), highest)
+		highest = max(energised(grid, node{[2]int{len(grid) - 1, i}, "U"}), highest)
+	}
+	fmt.Println("Part 2:", highest)
+}
+
+func energised(grid []string, start node) int {
+	queue := []node{start}
+	visited := make(map[[2]int][]string)
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+		if current.pos[0] < 0 || current.pos[0] >= len(grid) ||
+			current.pos[1] < 0 || current.pos[1] >= len(grid[0]) ||
+			includes(visited[current.pos], current.dir) {
+			continue
+		}
+		visited[current.pos] = append(visited[current.pos], current.dir)
+		mirror := grid[current.pos[0]][current.pos[1]]
+		switch current.dir {
+		case "U":
+			switch mirror {
+			case '\\':
+				queue = append(queue, node{[2]int{current.pos[0], current.pos[1] - 1}, "L"})
+			case '-':
+				queue = append(queue, node{[2]int{current.pos[0], current.pos[1] - 1}, "L"})
+				queue = append(queue, node{[2]int{current.pos[0], current.pos[1] + 1}, "R"})
+			case '/':
+				queue = append(queue, node{[2]int{current.pos[0], current.pos[1] + 1}, "R"})
+			default:
+				queue = append(queue, node{[2]int{current.pos[0] - 1, current.pos[1]}, "U"})
+			}
+		case "D":
+			switch mirror {
+			case '\\':
+				queue = append(queue, node{[2]int{current.pos[0], current.pos[1] + 1}, "R"})
+			case '-':
+				queue = append(queue, node{[2]int{current.pos[0], current.pos[1] - 1}, "L"})
+				queue = append(queue, node{[2]int{current.pos[0], current.pos[1] + 1}, "R"})
+			case '/':
+				queue = append(queue, node{[2]int{current.pos[0], current.pos[1] - 1}, "L"})
+			default:
+				queue = append(queue, node{[2]int{current.pos[0] + 1, current.pos[1]}, "D"})
+			}
+		case "L":
+			switch mirror {
+			case '\\':
+				queue = append(queue, node{[2]int{current.pos[0] - 1, current.pos[1]}, "U"})
+			case '|':
+				queue = append(queue, node{[2]int{current.pos[0] - 1, current.pos[1]}, "U"})
+				queue = append(queue, node{[2]int{current.pos[0] + 1, current.pos[1]}, "D"})
+			case '/':
+				queue = append(queue, node{[2]int{current.pos[0] + 1, current.pos[1]}, "D"})
+			default:
+				queue = append(queue, node{[2]int{current.pos[0], current.pos[1] - 1}, "L"})
+			}
+		case "R":
+			switch mirror {
+			case '\\':
+				queue = append(queue, node{[2]int{current.pos[0] + 1, current.pos[1]}, "D"})
+			case '|':
+				queue = append(queue, node{[2]int{current.pos[0] - 1, current.pos[1]}, "U"})
+				queue = append(queue, node{[2]int{current.pos[0] + 1, current.pos[1]}, "D"})
+			case '/':
+				queue = append(queue, node{[2]int{current.pos[0] - 1, current.pos[1]}, "U"})
+			default:
+				queue = append(queue, node{[2]int{current.pos[0], current.pos[1] + 1}, "R"})
+			}
+		}
+	}
+	return len(visited)
 }
 
 func includes(dirs []string, dir string) bool {
@@ -21,89 +104,4 @@ func includes(dirs []string, dir string) bool {
 		}
 	}
 	return false
-}
-
-func main() {
-	grid := strings.Split(strings.TrimSpace(input), "\n")
-	fmt.Println("Part 1:", energised(grid, node{[]int{0, 0}, "R"}))
-
-	highest := 0
-	for i := 0; i < len(grid); i++ {
-		highest = max(energised(grid, node{[]int{i, 0}, "R"}), highest)
-		highest = max(energised(grid, node{[]int{i, len(grid[0]) - 1}, "L"}), highest)
-	}
-	for i := 0; i < len(grid[0]); i++ {
-		highest = max(energised(grid, node{[]int{0, i}, "D"}), highest)
-		highest = max(energised(grid, node{[]int{len(grid) - 1, i}, "U"}), highest)
-	}
-	fmt.Println("Part 2:", highest)
-}
-
-func energised(grid []string, start node) int {
-	queue := []node{start}
-	visited := make(map[string][]string)
-	for len(queue) > 0 {
-		current := queue[0]
-		queue = queue[1:]
-		key := fmt.Sprintf("%v,%v", current.pos[0], current.pos[1])
-		if current.pos[0] < 0 || current.pos[0] >= len(grid) ||
-			current.pos[1] < 0 || current.pos[1] >= len(grid[0]) ||
-			includes(visited[key], current.dir) {
-			continue
-		}
-		visited[key] = append(visited[key], current.dir)
-		mirror := grid[current.pos[0]][current.pos[1]]
-		// fmt.Printf("Currently checking (%v, %v) = %s going %s\n", current.pos[0], current.pos[1], string(mirror), current.dir)
-		switch current.dir {
-		case "U":
-			switch mirror {
-			case '\\':
-				queue = append(queue, node{[]int{current.pos[0], current.pos[1] - 1}, "L"})
-			case '-':
-				queue = append(queue, node{[]int{current.pos[0], current.pos[1] - 1}, "L"})
-				queue = append(queue, node{[]int{current.pos[0], current.pos[1] + 1}, "R"})
-			case '/':
-				queue = append(queue, node{[]int{current.pos[0], current.pos[1] + 1}, "R"})
-			default:
-				queue = append(queue, node{[]int{current.pos[0] - 1, current.pos[1]}, "U"})
-			}
-		case "D":
-			switch mirror {
-			case '\\':
-				queue = append(queue, node{[]int{current.pos[0], current.pos[1] + 1}, "R"})
-			case '-':
-				queue = append(queue, node{[]int{current.pos[0], current.pos[1] - 1}, "L"})
-				queue = append(queue, node{[]int{current.pos[0], current.pos[1] + 1}, "R"})
-			case '/':
-				queue = append(queue, node{[]int{current.pos[0], current.pos[1] - 1}, "L"})
-			default:
-				queue = append(queue, node{[]int{current.pos[0] + 1, current.pos[1]}, "D"})
-			}
-		case "L":
-			switch mirror {
-			case '\\':
-				queue = append(queue, node{[]int{current.pos[0] - 1, current.pos[1]}, "U"})
-			case '|':
-				queue = append(queue, node{[]int{current.pos[0] - 1, current.pos[1]}, "U"})
-				queue = append(queue, node{[]int{current.pos[0] + 1, current.pos[1]}, "D"})
-			case '/':
-				queue = append(queue, node{[]int{current.pos[0] + 1, current.pos[1]}, "D"})
-			default:
-				queue = append(queue, node{[]int{current.pos[0], current.pos[1] - 1}, "L"})
-			}
-		case "R":
-			switch mirror {
-			case '\\':
-				queue = append(queue, node{[]int{current.pos[0] + 1, current.pos[1]}, "D"})
-			case '|':
-				queue = append(queue, node{[]int{current.pos[0] - 1, current.pos[1]}, "U"})
-				queue = append(queue, node{[]int{current.pos[0] + 1, current.pos[1]}, "D"})
-			case '/':
-				queue = append(queue, node{[]int{current.pos[0] - 1, current.pos[1]}, "U"})
-			default:
-				queue = append(queue, node{[]int{current.pos[0], current.pos[1] + 1}, "R"})
-			}
-		}
-	}
-	return len(visited)
 }
